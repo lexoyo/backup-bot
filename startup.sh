692d8a5dfc87a162e38a5328c293ef238b40abb9
Alex Hoyau <a.hoyau@silexlabs.org>
2024-07-17 11:56:40 +0200

    wip: debug docker startup script

--- startup.sh
+++ startup.sh
@@ -32,19 +32,17 @@ crontab -l
 
 # Write the cron.env file with the environment variables
 echo "Writing the cron.env file..."
-echo "CONFIG_YAML=\"$CONFIG_YAML\"" >> /app/cron.env
-echo "SSH_PRIVATE_KEY=\"$SSH_PRIVATE_KEY\"" >> /app/cron.env
-echo "S3_ENDPOINT=$S3_ENDPOINT" >> /app/cron.env
-echo "S3_BUCKET=$S3_BUCKET" >> /app/cron.env
-echo "S3_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID" >> /app/cron.env
-echo "S3_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY" >> /app/cron.env
-echo "S3_REGION=$S3_REGION" >> /app/cron.env
-echo "SMTP_SERVER=$SMTP_SERVER" >> /app/cron.env
-echo "SMTP_PORT=$SMTP_PORT" >> /app/cron.env
-echo "SMTP_USERNAME=$SMTP_USERNAME" >> /app/cron.env
-echo "SMTP_PASSWORD=$SMTP_PASSWORD" >> /app/cron.env
-echo "MAIL_FROM=$MAIL_FROM" >> /app/cron.env
-echo "MAIL_TO=$MAIL_TO" >> /app/cron.env
+echo "export S3_ENDPOINT=$S3_ENDPOINT" >> /app/cron.env
+echo "export S3_BUCKET=$S3_BUCKET" >> /app/cron.env
+echo "export S3_ACCESS_KEY_ID=$S3_ACCESS_KEY_ID" >> /app/cron.env
+echo "export S3_SECRET_ACCESS_KEY=$S3_SECRET_ACCESS_KEY" >> /app/cron.env
+echo "export S3_REGION=$S3_REGION" >> /app/cron.env
+echo "export SMTP_SERVER=$SMTP_SERVER" >> /app/cron.env
+echo "export SMTP_PORT=$SMTP_PORT" >> /app/cron.env
+echo "export SMTP_USERNAME=$SMTP_USERNAME" >> /app/cron.env
+echo "export SMTP_PASSWORD=$SMTP_PASSWORD" >> /app/cron.env
+echo "export MAIL_FROM=$MAIL_FROM" >> /app/cron.env
+echo "export MAIL_TO=$MAIL_TO" >> /app/cron.env
 
 # Run the app once at startup
 echo "Running the app at startup..."
